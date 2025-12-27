a902de20b4ed5e92950e6306b6585d4d11990ed2
TalhaShamo <talha246135@gmail.com>
2025-12-27 10:57:46 +0530

    Exception Handling + MiniProject

---
+++ MiniProject1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data;
+namespace MiniProject1
+{
+    interface IHealable
+    {
+        void Heal();
+    }
+    abstract class Fighter
+    {
+        private int _health;
+        public string Name {get; set;}
+        public Fighter(string n)
+        {
+            Name = n;
+        }
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    _health = 0;
+                }
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
+        public abstract void Attack(Fighter target);
+    }
+    class Warrior : Fighter
+    {
+        Random rnd = new Random();
+        public Warrior(string n) : base(n)
+        {
+            Health = 100;
+        }
+        public override void Attack(Fighter target)
+        {
+            int damage = rnd.Next(5, 15);
+            target.Health -= damage; //Randomly damages target for 5 to 15 damage
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} slashes {target.Name} for {damage}. Remaining health - {target.Health}hp!");
+        }
+    }
+    class Mage : Fighter, IHealable
+    {
+        Random rnd = new Random();
+        public Mage(string n) : base(n)
+        {
+            Health = 60;
+        }
+        public override void Attack(Fighter target)
+        {
+            int damage = rnd.Next(10, 20);
+            target.Health -= damage;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} casts fireball on {target.Name} for {damage}. Remaining health - {target.Health}hp!");
+        }
+        public void Heal()
+        {
+            Health += 10;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{Name} uses heal. Current hp : {Health}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior hulk = new Warrior("Hulk");
+            Mage loki = new Mage("Loki");
+            while(loki.Health > 0 && hulk.Health > 0)
+            {
+                hulk.Attack(loki);
+                if (loki.Health <= 0) break;
+                System.Threading.Thread.Sleep(3000);
+
+                Random rand = new Random();
+                int choice = rand.Next(0, 2);
+                if(choice == 1)
+                {
+                    loki.Attack(hulk);
+                }
+                else
+                {
+                    loki.Heal();
+                }
+                System.Threading.Thread.Sleep(3000);
+                if(hulk.Health <= 0) break;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            if(hulk.Health <= 0) Console.WriteLine($"Game Over! {loki.Name} wins 🎉");
+            else Console.WriteLine($"Game over! {hulk.Name} wins 🎉");
+        }
+    }
+}
---
+++ Module12_ExceptionHandling/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+namespace Module12_ExceptionHandling
+{
+    class Item
+    {
+        public string Name;
+        //Default Constructor
+        public Item()
+        {
+            Name = "Unknown Item";
+        }
+        // Parametrized Constructor
+        public Item(string n)
+        {
+            Name = n;
+        }
+    }
+    class Inventory
+    {
+        public List<Item> myInventory = new List<Item>(); //This is a list of objects from class Item.
+        public void GetMethod(int index)
+        {
+            try
+            {
+                Item listItem = myInventory[index];
+                Console.WriteLine($"Your Item : {listItem.Name}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something Went Wrong!");
+                Console.WriteLine($"Error - {ex}");
+            }
+            finally
+            {
+                Console.WriteLine("This block runs regardless of the outcome of both try and catch blocks");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item1 = new Item("Sword"); //Using the parameterized contructor
+            Item item2 = new Item(); //Using the default constructor
+
+            Inventory cart = new Inventory();
+            cart.myInventory.Add(item1);
+            cart.myInventory.Add(item2);
+
+            cart.GetMethod(1);
+            cart.GetMethod(10);
+        }
+    }
+}
