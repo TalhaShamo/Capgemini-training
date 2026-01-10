665b29711723eef758a06bead0e5a1e60e99e722
TalhaShamo <talha246135@gmail.com>
2026-01-10 09:49:16 +0530

    Generic classes and methods

---
+++ Module13_GenericClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Module13_GenericClasses
+{
+    class StorageBox<T>
+    {
+        public T Item;
+        public StorageBox(T item)
+        {
+            Item = item;
+        }
+        public void GetItem()
+        {
+            Console.WriteLine($"{Item}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Same class can be used to make a string item and int item. This is the power of a generic class. List<T> is also a type of generic
+            StorageBox<string> sBox = new StorageBox<string>("Hello");
+            StorageBox<int> iBox = new StorageBox<int>(100);
+            sBox.GetItem();
+            iBox.GetItem();
+        }
+    }
+}
---
+++ Module14_GenericMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Module14_GenericMethods
+{
+    class Utils
+    {
+        public void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greet = "Hello";
+            string name = "John";
+            Utils newUtil = new Utils();
+            Console.WriteLine($"Before Swapping : {greet} {name}");
+            newUtil.Swap<string>(ref greet, ref name);
+            Console.WriteLine($"After Swapping : {greet} {name}");
+
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine($"Before Swapping : {num1} {num2}");
+            newUtil.Swap<int>(ref num1, ref num2);
+            Console.WriteLine($"AFter Swappnig : {num1} {num2}");
+
+            //So in short we are able to swap different types of datatypes with the same method with the help of generic methods.
+            //This is inline wtih the DRY concept (Do not repeat yourself)
+        }
+    }
+}
